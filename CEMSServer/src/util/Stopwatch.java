99053f69c4c3e0047153cfbfb12db2f64c33050b
Tom Balmas <tombalmas12@gmail.com>
2021-06-09 01:22:25 +0300

    1:22 added timers and stuff

---
+++ CEMSServer/src/util/Stopwatch.java
@@ -0,0 +1,44 @@
+package util;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class Stopwatch {
+
+	private static final int MINUTE = 1000 * 60;
+	private static final int SECOND = 1000;
+	private int minutes;
+	private boolean startFlag = true;
+
+	public Stopwatch(int duration) {
+		this.minutes = duration;
+	}
+
+	public int getMinutes() {
+		return minutes;
+	}
+
+	public synchronized void startTimer(TimerTask tt) {
+		Timer minuteTimer = new Timer();
+		minuteTimer.scheduleAtFixedRate(new TimerTask() {
+			@Override
+			public void run() {
+				if (startFlag == true) {
+					minutes++;
+					startFlag = false;
+				}
+				minutes--;
+				System.out.println(minutes);
+				if (minutes <= 0) {
+					tt.run();
+					minuteTimer.cancel();
+				}
+			}
+		}, 0, MINUTE);
+	}
+
+	public synchronized void addMinutes(int minutesToAdd) {
+		minutes += minutesToAdd;
+	}
+
+}
